ae6b0be9bb2e63d6f2e649436f66c41f73e19791
lyx0 <stefan@parfuss.me>
2021-08-02 03:09:34 +0200

    add single file upload

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+
+// Helpers
+const methodOverride = require("method-override");
+const { Router } = require("express");
+
+// Multer
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+// MongoDB
+mongoose.connect("mongodb://localhost:27017/image-test", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection refused:"));
+db.once("open", () => {
+    console.log("Connected to database");
+});
+
+const app = express();
+
+// For EJS templates
+app.engine("ejs", ejsMate);
+
+app.set("view engine", "ejs");
+
+// So that the basepath is always the file with
+// app.js in it, regardless of where we call it
+app.set("views", path.join(__dirname, "views"));
+
+// Enables us to read the request body from the
+// POST request
+app.use(express.urlencoded({ extended: true }));
+
+// Enables us to use more than GET and POST in
+// a html request.
+app.use(methodOverride("_method"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use("/", uploadRoute);
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+    res.send("it worked");
+});
+
+app.listen(8080, () => {
+    console.log("Listening on port 8080");
+});
